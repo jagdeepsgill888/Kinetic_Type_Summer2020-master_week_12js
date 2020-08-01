788b320912ab54d4aa6547edb5b8a760225f77c2
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-08-01 00:15:16 -0400

    js with flip rough word

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
       typeAnim.to("#you",{ opacity:1, duration: 0.25})
                .to("#wanna", { opacity:1, duration: 0.3})
                .to("#play", { opacity:1, duration: 0.5})
-               .to(".type-wrapper", {  opacity:1, duration: 0.5, rotate:-90, scaleX:1.5, scaleY:1.5});
+               .to("#rough", { opacity:1, duration: 0.5})
+               .to("#typelayout", {  opacity:1, duration: 0.5, rotate:-90, scaleX:1.5, scaleY:1.5, x: -60, y: -300}, "<");
 
   function playAnimation() {
     typeAnim.play();
